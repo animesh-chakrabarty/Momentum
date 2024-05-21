8c6284efac6b6fbd25209f3a4061b65d244bc79d
animesh-chakrabarty <animesh_chakrabarty@outlook.com>
2024-05-22 02:32:00 +0530

    backend - user signup done

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -2,7 +2,14 @@ const User = require("../models/user.models");
 
 // POST - /signup
 const userSignup = async (req, res) => {
-  res.json({ msg: "signup user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // POST - /login
--- backend/src/models/user.models.js
+++ backend/src/models/user.models.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
   email: {
@@ -11,5 +12,23 @@ const userSchema = mongoose.Schema({
     required: true,
   },
 });
+
+// static signup method
+userSchema.statics.signup = async function (email, password) {
+  // checking if email already exists 
+  const doesExist = await this.findOne({ email });
+  if (doesExist) {
+    throw Error("Email already in use");
+  }
+
+  // generating salt
+  const salt = await bcrypt.genSalt(10);
+  // generating hash
+  const hash = await bcrypt.hash(password, salt);
+  // creating user doc in Users collection
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
